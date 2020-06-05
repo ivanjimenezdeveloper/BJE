a7896db215ac74584983e59fbdc805daafdb42d1
aa <ivanjl4444@hotmail.com>
2020-06-05 12:57:21 +0200

    comentario

--- src/controller/CreaHorario.java
+++ src/controller/CreaHorario.java
@@ -47,6 +47,13 @@ public class CreaHorario extends HttpServlet {
 	@EJB
 	HorarioEJB horarioEJB;
 
+	//____________________________________________________
+	//ESTA COMENTADO PORQUE EN UN FUTURO SE VA A USAR
+	//___________________________________________________
+	
+	
+	
+	
 //	/**
 //	 * Muestra el form de creacion de horarios generales
 //	 */
@@ -106,102 +113,102 @@ public class CreaHorario extends HttpServlet {
 //		}
 //	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		// recuperamos la sesion
-		HttpSession sesion = request.getSession(true);
-		// Obtenemos el usuario de la sesion si existe
-		Usuario user = sesionEJB.usuarioLogeado(sesion);
-		Usuario userHorario = null;
-		int mes = 0, anyo = 0, diasMaximo = 0;
-
-		// Recupero los atributos de creacion de horario
-		try {
-			userHorario = (Usuario) sesion.getAttribute("usuarioHorario");
-			mes = (int) sesion.getAttribute("mesHorario");
-			anyo = (int) sesion.getAttribute("anyoHorario");
-			diasMaximo = (int) sesion.getAttribute("diasMaximo");
-
-		} catch (Exception e) {
-
-			userHorario = null;
-
-		}
-
-		// recupera si esta o no en modo trabajo
-		int modoTrabajo;
-		try {
-			modoTrabajo = (int) sesion.getAttribute("modoTrabajo");
-
-		} catch (Exception e) {
-			modoTrabajo = 0;
-		}
-
-		// Comprueba que el usurio sea valido y si no redirige al main
-		if (user == null || user.getId() == 0 && user.getRol() == 0) {
-			response.sendRedirect("Main");
-
-		} else {
-			// Si el modo trabajo es 1 es que el modo trabajo esta activado y redirige al
-			// jsp del modo trabajo
-			if (modoTrabajo == 1) {
-				RequestDispatcher rs = getServletContext().getRequestDispatcher("/dist/indexTrabajo.jsp");
-				rs.forward(request, response);
-			} else {
-				// Segun el rol redirige al main o continua con la operacion
-				if (user.getRol() == 1) {
-
-					response.sendRedirect("Main");
-				} else if (user.getRol() == 2 || user.getRol() == 3) {
-					// comprueba que el usuario, mes y año sean validos
-					if (userHorario != null || userHorario.getId() != 0 || mes != 0 || anyo != 0) {
-
-						String fecha = "";
-						int existe = 0;
-
-						// por cada dia segun el mes dado se hace una pasada
-						for (int i = 1; i <= diasMaximo; i++) {
-							// reinicio el objeto diaAdd
-							Dia diaAdd = new Dia();
-
-							// añade las entradas y salidas con el formato 00, es decir si no hay 2 numeros
-							// añadira un 0 a la izquierda
-							diaAdd.setEntrada_1(request.getParameter("entrada1" + String.format("%02d", i)));
-							diaAdd.setEntrada_2(request.getParameter("entrada2" + String.format("%02d", i)));
-							diaAdd.setSalida_1(request.getParameter("salida1" + String.format("%02d", i)));
-							diaAdd.setSalida_2(request.getParameter("salida2" + String.format("%02d", i)));
-							fecha = String.format("%02d", anyo) + "-" + String.format("%02d", mes) + "-"
-									+ String.format("%02d", i);
-							
-							diaAdd.setFecha(fecha);
-
-							diaAdd.setUsuario(userHorario.getId());
-
-							//comprueba si existe el dia con asociado a este usuario
-							existe = diaEJB.existeDia(userHorario, fecha);
-
-							//si el dia es valido lo inserta si no, no hace nada
-							if (!diaAdd.getEntrada_1().equals("") && !diaAdd.getEntrada_1().equals("") && existe == 0) {
-								Horario h = horarioEJB.horarioIdPorMesAnyo(mes, anyo);
-
-								diaEJB.insertarDia(diaAdd, h.getId());
-							}
-
-						}
-
-						response.sendRedirect("Main");
-
-					}
-				} else {
-
-					response.sendRedirect("Main");
-
-				}
-			}
-
-		}
-
-	}
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//			throws ServletException, IOException {
+//
+//		// recuperamos la sesion
+//		HttpSession sesion = request.getSession(true);
+//		// Obtenemos el usuario de la sesion si existe
+//		Usuario user = sesionEJB.usuarioLogeado(sesion);
+//		Usuario userHorario = null;
+//		int mes = 0, anyo = 0, diasMaximo = 0;
+//
+//		// Recupero los atributos de creacion de horario
+//		try {
+//			userHorario = (Usuario) sesion.getAttribute("usuarioHorario");
+//			mes = (int) sesion.getAttribute("mesHorario");
+//			anyo = (int) sesion.getAttribute("anyoHorario");
+//			diasMaximo = (int) sesion.getAttribute("diasMaximo");
+//
+//		} catch (Exception e) {
+//
+//			userHorario = null;
+//
+//		}
+//
+//		// recupera si esta o no en modo trabajo
+//		int modoTrabajo;
+//		try {
+//			modoTrabajo = (int) sesion.getAttribute("modoTrabajo");
+//
+//		} catch (Exception e) {
+//			modoTrabajo = 0;
+//		}
+//
+//		// Comprueba que el usurio sea valido y si no redirige al main
+//		if (user == null || user.getId() == 0 && user.getRol() == 0) {
+//			response.sendRedirect("Main");
+//
+//		} else {
+//			// Si el modo trabajo es 1 es que el modo trabajo esta activado y redirige al
+//			// jsp del modo trabajo
+//			if (modoTrabajo == 1) {
+//				RequestDispatcher rs = getServletContext().getRequestDispatcher("/dist/indexTrabajo.jsp");
+//				rs.forward(request, response);
+//			} else {
+//				// Segun el rol redirige al main o continua con la operacion
+//				if (user.getRol() == 1) {
+//
+//					response.sendRedirect("Main");
+//				} else if (user.getRol() == 2 || user.getRol() == 3) {
+//					// comprueba que el usuario, mes y año sean validos
+//					if (userHorario != null || userHorario.getId() != 0 || mes != 0 || anyo != 0) {
+//
+//						String fecha = "";
+//						int existe = 0;
+//
+//						// por cada dia segun el mes dado se hace una pasada
+//						for (int i = 1; i <= diasMaximo; i++) {
+//							// reinicio el objeto diaAdd
+//							Dia diaAdd = new Dia();
+//
+//							// añade las entradas y salidas con el formato 00, es decir si no hay 2 numeros
+//							// añadira un 0 a la izquierda
+//							diaAdd.setEntrada_1(request.getParameter("entrada1" + String.format("%02d", i)));
+//							diaAdd.setEntrada_2(request.getParameter("entrada2" + String.format("%02d", i)));
+//							diaAdd.setSalida_1(request.getParameter("salida1" + String.format("%02d", i)));
+//							diaAdd.setSalida_2(request.getParameter("salida2" + String.format("%02d", i)));
+//							fecha = String.format("%02d", anyo) + "-" + String.format("%02d", mes) + "-"
+//									+ String.format("%02d", i);
+//							
+//							diaAdd.setFecha(fecha);
+//
+//							diaAdd.setUsuario(userHorario.getId());
+//
+//							//comprueba si existe el dia con asociado a este usuario
+//							existe = diaEJB.existeDia(userHorario, fecha);
+//
+//							//si el dia es valido lo inserta si no, no hace nada
+//							if (!diaAdd.getEntrada_1().equals("") && !diaAdd.getEntrada_1().equals("") && existe == 0) {
+//								Horario h = horarioEJB.horarioIdPorMesAnyo(mes, anyo);
+//
+//								diaEJB.insertarDia(diaAdd, h.getId());
+//							}
+//
+//						}
+//
+//						response.sendRedirect("Main");
+//
+//					}
+//				} else {
+//
+//					response.sendRedirect("Main");
+//
+//				}
+//			}
+//
+//		}
+//
+//	}
 
 }
